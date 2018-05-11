77783653c246c2e2a4e91fa33332e630ce41e3f8
Javier Claros - Mi Laptop <javiclaros@gmail.com>
2018-05-11 18:06:33 -0400

    Add hashing example, edit Users Model and Add POST /users

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,33 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
 
 var User = mongoose.model('User', {
   email: {
     type: String,
-    required: true,
+    require: true,
     minlength: 1,
-    trim: true
-  }
+    trim: true,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: '{VALUE} is not a valid email'
+    }
+  },
+  password: {
+    type: String,
+    require: true,
+    minlength: 6
+  },
+  tokens: [{
+    access: {
+      type: String,
+      require: true
+    },
+    token: {
+      type: String,
+      require: true
+    }
+  }]
 });
 
 module.exports = { User };
