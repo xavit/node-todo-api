4bc2b36b729c455ed33b26b18538b1534d1e993b
Javier Claros - Mi Laptop <javiclaros@gmail.com>
2018-05-16 22:12:16 -0400

    Hash user password before saving

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 var UserSchema = new mongoose.Schema({
   email: {
@@ -68,22 +69,21 @@ UserSchema.statics.findByToken = function (token) {
     'tokens.access': 'auth'
   });
 };
-// UserSchema.statics.findByToken = function (token) {
-//   var User = this;
-//   var decoded;
 
-//   try{
-//     decoded = jwt.verify(token, 'abc123');
-//   } catch (e) {
+UserSchema.pre('save', function (next) {
+  var user = this;
 
-//   }
-
-//   return User.findOne({
-//     '_id': decoded._id,
-//     'tokens.token': token,
-//     'tokens.access': 'auth'
-//   });
-// };
+  if (user.isModified('password')) {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
 
 var User = mongoose.model('User', UserSchema);
 
